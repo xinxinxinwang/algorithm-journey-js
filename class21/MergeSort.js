38d6ff622929cfc37c51ea93b456f842fa53c8f8
wangxin <347720427@qq.com>
2025-12-29 16:01:12 +0800

    merge sort

--- class21/MergeSort.js
+++ class21/MergeSort.js
@@ -33,6 +33,7 @@ var sortArray = function (nums) {
         while (b <= r) {
             help[i++] = nums[b++]
         }
+        //i的范围是l到r
         for (i = l; i <= r; i++) {
             nums[i] = help[i]
         }
@@ -45,4 +46,47 @@ var sortArray = function (nums) {
     return nums;
 };
 
-console.log(sortArray([8, 4, 5, 10, 2, 3, 4]))
+//非递归方法
+
+var sortArray = function (nums) {
+    if (nums.length < 1) {
+        return nums;
+    }
+    //help是全局的辅助数组
+    let help = new Array(nums.length);
+
+    const mergeArr = function (l, m, r) {
+        let a = l;
+        let b = m + 1;
+        //i 是从l开始
+        let i = l;
+        while (a <= m && b <= r) {
+            help[i++] = nums[a] <= nums[b] ? nums[a++] : nums[b++]
+        }
+        while (a <= m) {
+            help[i++] = nums[a++]
+        }
+        while (b <= r) {
+            help[i++] = nums[b++]
+        }
+        //i的范围是l到r
+        for (i = l; i <= r; i++) {
+            nums[i] = help[i]
+        }
+    }
+    let n = nums.length;
+    for (let l, m, r, step = 1; step < n; step <<= 1) {
+        l = 0;
+        while (l < n) {
+            m = l + step - 1
+            if (m + 1 >= n) {
+                break;
+            }
+            r = Math.min((l + (step << 1) - 1), n - 1)
+            mergeArr(l, m, r);
+            l = r + 1;
+
+        }
+    }
+    return nums;
+};
